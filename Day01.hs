0e51498f22c55dac65b5bb779cd56a6842390fcd
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-01 09:08:40 +0100

    Steal this gem https://twitter.com/GabriellaG439/status/701460899589017600

--- Day01.hs
+++ Day01.hs
@@ -1,9 +1,14 @@
+import Data.List (transpose, tails)
+
 part1 :: [Int] -> Int
 part1 terms = do
     length $ filter (uncurry (>)) $ zip (tail terms) terms
 
 part2 :: [Int] -> Int
-part2 terms = part1 $ zipWith (+) (tail $ tail terms) (zipWith (+) (tail terms) terms)
+part2 terms = part1 $ map sum $ windows 3 terms
+
+windows :: Int -> [a] -> [[a]]
+windows n = transpose . take n . tails
 
 main = do
     terms <- map read . lines <$> readFile "day01.txt"
