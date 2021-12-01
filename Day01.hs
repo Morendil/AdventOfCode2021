d2a2967660e5b5e849835e4c81c094ed836843b7
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-01 09:03:19 +0100

    Nettoyage…

--- Day01.hs
+++ Day01.hs
@@ -1,11 +1,11 @@
+part1 :: [Int] -> Int
 part1 terms = do
-    print $ length $ filter (uncurry (>)) $ zip (tail terms) terms
+    length $ filter (uncurry (>)) $ zip (tail terms) terms
 
+part2 :: [Int] -> Int
 part2 terms = part1 $ zipWith (+) (tail $ tail terms) (zipWith (+) (tail terms) terms)
 
 main = do
-    contents <- readFile "day01.txt"
-    let terms :: [Int]
-        terms = map read $ lines contents
-    part1 terms
-    part2 terms
+    terms <- map read . lines <$> readFile "day01.txt"
+    print $ part1 terms
+    print $ part2 terms
