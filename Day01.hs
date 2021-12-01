908d9cd6ef7395cff908b98f219f79c70414f23a
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-01 08:41:16 +0100

    Day 1 part 2

--- Day01.hs
+++ Day01.hs
@@ -1,8 +1,11 @@
 part1 terms = do
     print $ length $ filter (uncurry (>)) $ zip (tail terms) terms
 
+part2 terms = part1 $ zipWith (+) (tail $ tail terms) (zipWith (+) (tail terms) terms)
+
 main = do
     contents <- readFile "day01.txt"
     let terms :: [Int]
         terms = map read $ lines contents
     part1 terms
+    part2 terms
