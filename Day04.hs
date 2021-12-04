2b0ebe582e8492c701be92b30c4274a8e266450c
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-04 12:35:55 +0100

    Day 4 part 1

---
+++ Day04.hs
@@ -0,0 +1,48 @@
+import Text.ParserCombinators.ReadP
+import Data.Char
+import Data.List
+import Data.Maybe
+
+type Board = [[Int]]
+type Called = [Int]
+
+number :: ReadP Int
+number = read <$> many1 (satisfy isNumber)
+numbers :: ReadP [Int]
+numbers = sepBy1 number (string ",")
+row :: ReadP [Int]
+row = sepBy1 (do; d1 <- get; d2 <- get; return $ read [d1,d2]) (string " ") 
+board :: ReadP [[Int]]
+board = sepBy1 row (string "\n")
+
+bingo :: ReadP ([Int], [Board])
+bingo = do
+    draws <- numbers
+    string "\n\n"
+    boards <- sepBy1 board (string "\n\n")
+    return (draws, boards)
+
+parse :: ReadP a -> String ->  a
+parse parser input =
+    case reverse $ readP_to_S parser input of
+        ((result, _):_) -> result
+        _ -> error "No parse"
+
+wins :: Called -> Board -> Bool
+wins called board = winByRow || winByCol
+    where full row = all (`elem` called) row
+          winByRow = any full board
+          winByCol = any full (transpose board)
+
+score :: Called -> Board -> Int
+score called board = sum (concat board \\ called) * last called
+
+winner :: [Board] -> Called -> Maybe Int
+winner boards called = if null winning then Nothing else Just $ score called (head winning)
+    where winning = filter (wins called) boards
+
+main = do
+    (called, boards) <- parse bingo <$> readFile "day04.txt"
+    let winnerScore = take 1 $ catMaybes $ map (winner boards) (tail $ inits called)
+    -- part1
+    print winnerScore
