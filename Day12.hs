548bf48c45b706f0f060dca329dd452b0a3e3b50
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-12 11:18:21 +0100

    Day 12 part 2

--- Day12.hs
+++ Day12.hs
@@ -11,15 +11,26 @@ pair = map unpack . splitOn (pack "-") . pack
 toEdges pair@[from,to] = [(from,to),(to,from)]
 toEdges _ = error "Pairs only"
 
-paths :: [Edge] -> [Path] -> [Node] ->  Node -> [Path]
-paths _ starts _ "end" = map (++ ["end"]) starts
-paths edges starts visited current = concatMap (paths edges starts' visited') next
+paths1 :: [Edge] -> [Path] -> [Node] ->  Node -> [Path]
+paths1 _ starts _ "end" = map (++ ["end"]) starts
+paths1 edges starts visited current = concatMap (paths1 edges starts' visited') next
     where next = map snd $ filter (not.(`elem` visited).snd) $ filter ((current ==).fst) edges
           visited' = if all isLower current then current:visited else visited
           starts' = map (++[current]) starts
 
+paths2 :: [Edge] -> [Path] -> [Node] ->  [Node] -> Node -> [Path]
+paths2 _ starts _ _ "end" = map (++ ["end"]) starts
+paths2 edges starts visited twice current = concatMap (paths2 edges starts' visited' twice') next
+    where next = map snd $ filter (canReach.snd) $ filter ((current ==).fst) edges
+          starts' = map (++[current]) starts
+          visited' = if all isLower current then current:visited else visited
+          canReach cave = null twice' || cave `notElem` visited
+          twice' = if null twice && current `elem` visited && all isLower current then [current] else twice
+
 main = do
     pairs <- map pair . lines <$> readFile "day12.txt"
-    let edges = concatMap toEdges pairs
+    let edges = filter ((/="end").fst) $ filter ((/="start").snd) $ concatMap toEdges pairs
     -- part1
-    print $ length $ paths edges [[]] [] "start"
+    print $ length $ paths1 edges [[]] [] "start"
+    -- part2
+    print $ length $ paths2 edges [[]] [] [] "start"
