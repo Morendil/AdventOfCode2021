e629aa15cc8d8e5031795094d4ff3287f63f98d4
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-15 00:15:50 +0100

    Day 14… enter the Matrix

--- Day14.hs
+++ Day14.hs
@@ -3,6 +3,9 @@ import Data.Maybe
 import Data.List
 import qualified Data.Map as M
 
+import Data.Matrix
+import qualified Data.Vector as V
+
 type Rule = (String, String)
 type Pair = (Char, Char)
 type Pairs = M.Map Pair Int
@@ -14,35 +17,41 @@ split sep = map unpack . splitOn (pack sep) . pack
 toPair [x,y] = (x,y)
 toPair _ = error "Nope"
 
-expand :: [Rule] -> Pairs -> Pairs
-expand rules poly = foldr ($) poly transforms
-    where transforms = map (applyRule poly) rules
+generator :: [Rule] -> (Int, Int) -> Int
+generator rules (y,x)
+  | y == left = 1
+  | y == right = 1
+  | otherwise = 0
+  where
+      elements = sort $ nub $ concatMap fst rules
+      pairs = [[x, y] | x <- elements, y <- elements]
+      before@[a, b] = pairs !! (x-1)
+      insert = head $ fromJust $ lookup before rules
+      left = 1 + fromJust (elemIndex [a, insert] pairs)
+      right = 1 + fromJust (elemIndex [insert, b] pairs)
 
-applyRule :: Pairs -> Rule -> (Pairs -> Pairs)
-applyRule pairs ([a,b],[c]) = case M.lookup (a,b) pairs of
-    Nothing -> id
-    Just n -> M.alter (Just . maybe n (+n)) (a,c) . M.alter (Just . maybe n (+n)) (c,b) . M.adjust (\x->x-n) (a,b)
-applyRule _ _ = error "foo"
+vector :: [Rule] -> String -> [Int]
+vector rules poly = map (count poly) pairs
+    where count s t = sum [ 1 | r <- tails s, t `isPrefixOf` r ]
+          elements = sort $ nub $ concatMap fst rules
+          pairs = [[x, y] | x <- elements, y <- elements]
 
-insertPair :: Char -> Char -> Pairs -> Pairs
-insertPair a b = M.alter (Just . maybe 1 succ) (a,b)
-
-initial :: String -> Pairs
-initial poly = foldr ($) M.empty insertions
-    where insertions = zipWith insertPair poly (tail poly)
-
-count :: Pairs -> Freqs
-count = M.foldrWithKey (\(a,b) n -> M.alter (Just . maybe n (+n)) b) M.empty
-
-solve :: [Rule] -> String -> Int -> Int
-solve rules template n = most - least
-    where expanded = last $ take (n+1) $ iterate (expand rules) (initial template)
-          counted = M.adjust (+1) (head template) $ count expanded
-          most = maximum $ M.elems counted
-          least = minimum $ M.elems counted
+solveMatrix :: [Rule] -> String -> Int -> Int
+solveMatrix rules template n = maximum counts - minimum counts
+    where elements = sort $ nub $ concatMap fst rules
+          pairs = [[x, y] | x <- elements, y <- elements]
+          size = length pairs
+          m = matrix size size (generator rules)
+          initial = V.fromList $ vector rules template
+          repeated = foldr1 multStd (replicate n m)
+          final = multStd repeated (colVector initial)
+          pairCounts = zip pairs (V.toList $ getMatrixAsVector final)
+          counts = map (\e -> fromEnum (e == head template) + sum (map snd $ filter (\p -> last (fst p) == e) pairCounts)) elements
 
 main = do
     [template, rest] <- split "\n\n" <$> readFile "day14.txt"
     let rules = map (toPair . split " -> ") $ lines rest
-    print $ solve rules template 10
-    print $ solve rules template 40
+        elements = sort $ nub $ concatMap fst rules
+        pairs = [[x, y] | x <- elements, y <- elements]
+    print $ solveMatrix rules template 10
+    print $ solveMatrix rules template 40
