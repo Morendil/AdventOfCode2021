e825d94b105365de4fd84ae59d30a46f4deff6fa
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-16 18:38:34 +0100

    Debug all the things, to no avail

--- Day16.hs
+++ Day16.hs
@@ -4,6 +4,7 @@ import Data.Char
 import Data.Maybe
 import Test.Hspec
 import Test.QuickCheck
+import Debug.Trace
 
 data Packet = Literal {version :: Int, value :: Int} | Operator {version :: Int, typeId :: Int, packets :: [Packet]}
     deriving (Eq, Show)
@@ -12,23 +13,44 @@ versionSum :: Packet -> Int
 versionSum Literal {version=version} = version
 versionSum Operator {version=version, packets=packets} = version + sum (map versionSum packets)
 
-bITS :: ReadP Packet
+bITS :: ReadP (Int, Packet)
 bITS = literalPacket <++ operatorPacket
 
-literalPacket :: ReadP Packet
+literalPacket :: ReadP (Int, Packet)
 literalPacket = do
     version <- bits 3
     string "100"
     (len, literal) <- literalValue
-    return Literal {version=version, value=literal}
+    return (len+6, Literal {version=version, value=literal})
 
-operatorPacket :: ReadP Packet
+operatorPacket :: ReadP (Int, Packet)
 operatorPacket = do
     version <- bits 3
     typeId <- bits 3
-    length <- (string "0" *> bits 15) <++ (string "1" *> bits 11)
-    packets <- many1 bITS
-    return Operator {version=version, typeId=typeId, packets=packets}
+    lenPacket version typeId <++ numPacket version typeId
+
+numPacket :: Int -> Int -> ReadP (Int, Packet)
+numPacket version typeId = do
+    string "1"
+    num <- bits 11
+    pairs <- count (traceShow ("num:"++show num) num) bITS
+    let total = sum $ map fst $ traceShow ("num:"++show pairs) pairs
+        packets = map snd pairs
+    return (total+6, Operator {version=traceShow ("v:"++show version) version, typeId=typeId, packets=packets})
+
+lenPacket :: Int -> Int -> ReadP (Int, Packet)
+lenPacket version typeId = do
+    string "0"
+    length <- bits 15
+    let doPackets :: Int -> [(Int, Packet)] -> ReadP [(Int, Packet)]
+        doPackets n pairs = if n >= length then return $ reverse pairs
+            else do
+                pair@(len, packet) <- bITS
+                doPackets (n+len) (pair:pairs)
+    pairs <- doPackets 0 []
+    let total = sum $ map fst $ traceShow ("len:"++show pairs) pairs
+        packets = map snd pairs
+    return (traceShowId $ total+6, Operator {version=version, typeId=typeId, packets=packets})
 
 bits :: Int -> ReadP Int
 bits n = toDecimal . map digitToInt <$> count n (satisfy isDigit)
@@ -38,7 +60,7 @@ toDecimal s = sum $ zipWith (\a b -> a * (2 ^ b)) (reverse s) [0..]
 
 literalValue :: ReadP (Int, Int)
 literalValue = consume <$> collect
-    where consume s = (5 * (length s `div` 4), toDecimal $ map digitToInt s)
+    where consume s = (5 * (length s `div` 4), toDecimal $ map digitToInt (traceShow ("literal:"++s) s))
           one = count 4 (satisfy isDigit)
           collect = stop <++ continue
           stop = string "0" *> one
@@ -55,6 +77,9 @@ main = do
     packet <- parseMaybe bITS . fromHex <$> readFile "day16.txt"
     print $ fromJust packet
 
+parse :: String -> Packet
+parse = snd . fromJust . parseMaybe bITS . fromHex
+
 parseMaybe :: ReadP a -> String -> Maybe a
 parseMaybe parser input =
     case reverse $ readP_to_S parser input of
@@ -64,17 +89,17 @@ parseMaybe parser input =
 test :: IO ()
 test = hspec $ do
   describe "Parser" $ do
-    it "succeeds on literal value" $
-        fromJust (parseMaybe bITS $ fromHex "D2FE28") `shouldBe` Literal {version=6, value=2021}
-    it "succeeds on 2-packet operator value" $
-        fromJust (parseMaybe bITS $ fromHex "38006F45291200") `shouldBe` Operator {version=1, typeId=6, packets=[Literal {version=6, value=10},Literal {version=2, value=20}]}
-    it "succeeds on 3-packet operator value" $
-        fromJust (parseMaybe bITS $ fromHex "EE00D40C823060") `shouldBe` Operator {version=7, typeId=3, packets=[Literal {version=2, value=1},Literal {version=4, value=2},Literal {version=1, value=3}]}
-    it "computes the correct sum, 16" $
-        versionSum (fromJust (parseMaybe bITS $ fromHex "620080001611562C8802118E34")) `shouldBe` 12
-    it "computes the correct sum, 16" $
-        versionSum (fromJust (parseMaybe bITS $ fromHex "8A004A801A8002F478")) `shouldBe` 16
+    -- it "succeeds on literal value" $
+    --     parse "D2FE28" `shouldBe` Literal {version=6, value=2021}
+    -- it "succeeds on 2-packet operator value" $
+    --     parse "38006F45291200" `shouldBe` Operator {version=1, typeId=6, packets=[Literal {version=6, value=10},Literal {version=2, value=20}]}
+    -- it "succeeds on 3-packet operator value" $
+    --     parse "EE00D40C823060" `shouldBe` Operator {version=7, typeId=3, packets=[Literal {version=2, value=1},Literal {version=4, value=2},Literal {version=1, value=3}]}
+    -- it "computes the correct sum, 12" $
+    --     versionSum (parse "620080001611562C8802118E34") `shouldBe` 12
+    -- it "computes the correct sum, 16" $
+    --     versionSum (parse "8A004A801A8002F478") `shouldBe` 16
     it "computes the correct sum, 23" $
-        versionSum (fromJust (parseMaybe bITS $ fromHex "C0015000016115A2E0802F182340")) `shouldBe` 23
-    it "computes the correct sum, 31" $
-        versionSum (fromJust (parseMaybe bITS $ fromHex "A0016C880162017C3686B18A3D4780")) `shouldBe` 31
+        versionSum (parse "C0015000016115A2E0802F182340") `shouldBe` 23
+    -- it "computes the correct sum, 31" $
+    --     versionSum (parse "A0016C880162017C3686B18A3D4780") `shouldBe` 31
