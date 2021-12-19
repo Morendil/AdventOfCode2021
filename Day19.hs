a6e3979a01c1d6ec99395e97fc4669bdcd27bf4d
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-19 23:38:06 +0100

    Day 19… finally gets the sample right

--- Day19.hs
+++ Day19.hs
@@ -27,7 +27,7 @@ triplet :: String -> [Int]
 triplet = map read . split ","
 
 orientations :: [Orientation]
-orientations = [zip d p | p <- axesPermutations, d <- directions]
+orientations = nub [zip d p | p <- axesPermutations, d <- directions]
     where axesPermutations = permutations [0,1,2]
           directions = replicateM 3 [1,-1]
 
@@ -45,7 +45,7 @@ enrich :: Scanner -> Enriched
 enrich scanner = (scanner, distances scanner)
 
 match :: Distances -> Scanner -> Maybe (Orientation, Point)
-match reference scanner = if length matches < 66 then Nothing else Just (adjust $ head candidates)
+match reference scanner = if length matches /= 66 then Nothing else Just (adjust $ head candidates)
     where sameDist a b = fst a == fst b
           dists = distances scanner
           matches = intersectBy sameDist dists reference
@@ -60,28 +60,45 @@ match reference scanner = if length matches < 66 then Nothing else Just (adjust 
           oldAxes = axes fromRef
           delta old new = nub $ zipWith (-) old new
 
+match2 reference scanner = if length matches /= 66 then Nothing else Just candidates
+    where sameDist a b = fst a == fst b
+          dists = distances scanner
+          matches = intersectBy sameDist dists reference
+          fromRef = points $ intersectBy sameDist reference matches
+          fromNew = points matches
+          points = nub . concatMap ((\(a,b)->[a,b]).snd)
+          candidates = filter good $ map (\o -> (o, try $ transform fromNew o)) orientations
+          good (o, trial) = all ((==1) . length) trial
+          adjust (o, trial) = (o, map head trial)
+          try points = zipWith delta oldAxes (axes points)
+          axes = map sort . transpose
+          oldAxes = axes fromRef
+          delta old new = nub $ zipWith (-) old new
 initial :: [Scanner] -> Solution
 initial scanners = M.insert 0 (head scanners, [0,0,0]) M.empty
 
 solveStep :: [Scanner] -> (Solution, [(Int, Int)]) -> (Solution, [(Int, Int)])
 solveStep scanners (solution, pairs) = (newSolution, remainingPairs)
     where candidatePairs = filter (\(a,b) -> isJust $ M.lookup a solution) pairs
-          remainingPairs = filter (\(a,b) -> isNothing $ M.lookup b newSolution) pairs
-          dists = map (\n -> distances $ scanners !! n) (nub $ map fst candidatePairs)
+          remainingPairs = filter (\(a,b) -> isNothing $ M.lookup b newSolution) (pairs \\ candidatePairs)
+          distMap = M.fromList $ map (\n -> (n, distances $ fst $ fromJust $ M.lookup n solution)) (nub $ map fst candidatePairs)
           newSolution = foldl solvePair solution candidatePairs
           solvePair solution (from, to) = if isNothing found then solution else solution'
-              where found = match (dists !! from) (scanners !! to)
+              where found = match (fromJust $ M.lookup from distMap) (scanners !! to)
+                    (o, p) = fromJust found
                     (_, origin) = fromJust $ M.lookup from solution
                     solution' = M.insert to solved solution
-                    (o, p) = fromJust found
-                    solved = (transform (scanners !! to) o, add p origin)
+                    solved = (map (add p) $ transform (scanners !! to) o, p)
 
 solved :: (Solution, [(Int, Int)]) -> Bool
 solved (_, []) = True
 solved _ = False
 
 main = do
-    scanners <- map (map triplet . tail . lines) . split "\n\n" <$> readFile "day19.txt"
-    let (solution,_) = last $ takeUntil solved $ iterate (solveStep scanners) (initial scanners, allPairs [0..length scanners-1])
+    scanners <- map (map triplet . tail . lines) . split "\n\n" <$> readFile "day19_test.txt"
+    let (solution,_) = last $ takeUntil solved $ iterate (solveStep scanners) (initial scanners, [(x,y) | x <- [0..length scanners-1], y <- [0..length scanners-1], x/=y])
         beacons = nub $ concatMap fst $ M.elems solution
+    print $ M.keys solution
+    print $ map snd $ M.elems solution
     print $ length beacons
+    -- putStrLn $ unlines $ map show $ sort beacons
